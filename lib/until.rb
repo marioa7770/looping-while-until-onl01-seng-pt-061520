fc270abe7858200cea2e53f2cc1d41410dbd06ac
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-29 01:13:24 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
-  levitation_force = 6
-  #your code here
-    
+  
+	levitation_force = 6
+	while levitation_force < 10
+	levitation_force += 1
+		puts "Wingardium Leviosa"
+	
+	end
+	puts "You did it! The feather is levitating!"
 end
-
